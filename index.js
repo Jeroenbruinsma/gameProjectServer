e14a40b468e1a637bca1c2440610297dc384491f
Jeroen Bruinsma <jeroenbruinsma@gmail.com>
2019-07-08 14:55:39 +0200

    updated package.json start and dev command

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser")
+const jsonParser = bodyParser.json()
+let port;
+require("./db.js")
+const advertisement = require('./advertisement/router')
+//const tokens = require('./tokens/middleware')
+const cors = require('cors')
+
+if(!process.env.PORT){
+     port = 5000
+}else{
+     port = process.env.PORT
+}
+    
+console.log("Api server on port", port)
+app.get('/', (req, res) => res.send('Server running'))
+
+app.listen(port, () => `Listening on port ${port}`)
+app.use(cors())
+app.use(jsonParser)
+//app.use(advertisement)
